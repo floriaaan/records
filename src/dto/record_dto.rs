7d0161bc0ef308941b10272381d6c650f5b52e66
Florian LEROUX <florian.leroux3@laposte.net>
2025-03-25 13:22:44 +0100

    feat: amélioration de la validation des entrées utilisateur et mise à jour des mots de passe

--- src/dto/record_dto.rs
+++ src/dto/record_dto.rs
@@ -3,21 +3,24 @@ use validator::{Validate, ValidationError};
 
 #[derive(Deserialize, Serialize, FromForm, Debug, Validate)]
 pub struct RecordInput {
-    #[validate(length(min = 1))]
+    #[validate(length(min = 1, message = "Title is required"))]
     pub title: String,
 
-    #[validate(length(min = 1))]
+    #[validate(length(min = 1, message = "Artist is required"))]
     pub artist: String,
 
-    #[validate(length(min = 8))]
+    #[validate(length(
+        min = 8,
+        message = "Release date must be at least 8 characters long (e.g. 2025-01-01)"
+    ))]
     pub release_date: String,
 
-    #[validate(url)]
+    #[validate(url(message = "Cover URL is not a valid URL"))]
     pub cover_url: String,
 
-    #[validate(url)]
+    #[validate(url(message = "Discogs URL is not a valid URL"))]
     pub discogs_url: Option<String>,
 
-    #[validate(url)]
+    #[validate(url(message = "Spotify URL is not a valid URL"))]
     pub spotify_url: Option<String>,
 }
